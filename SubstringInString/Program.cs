b73c4771138608f1877d80192f0b5cdf0e580cfe
larisa33 <muntoiu.larisa@gmail.com>
2023-10-09 15:48:20 +0300

    substring in string

---
+++ SubstringInString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlTypes;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace SubstringInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string initialText = "Ana are mere. Merele sunt rosii.";
+            Console.WriteLine(initialText);
+
+            string subString;
+            Console.Write("Introdu substringul: ");
+            subString = Console.ReadLine();
+
+            int count = Regex.Matches(initialText.ToLower(), subString).Count();
+
+            Console.WriteLine("Substringul a aparut de " + count + " ori in text");
+
+        }
+    }
+}
